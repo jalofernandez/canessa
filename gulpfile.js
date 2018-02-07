f4191f2c4c6ea635905f42aaa4a0c0a7d0a30970
jalofernandez <jalofernandez@gmail.com>
2018-02-07 13:12:14 +0100

    Project goes from Github Pages to Firebase

--- gulpfile.js
+++ gulpfile.js
@@ -29,10 +29,10 @@ gulp.task('dev', ['less', 'css', 'js', 'mincss', 'pug']); // task order is not r
 * Configuración de la tarea 'js' --> gulp-concat + gulp-uglify (gulp js)
 */
 gulp.task('js', function () {
-  gulp.src('js/sources/*.js')
+  gulp.src('js/*.js')
   .pipe(concat('scripts-min.js'))
   .pipe(uglify())
-  .pipe(gulp.dest('js/dist/'))
+  .pipe(gulp.dest('./public/js'))
 });
 
 /*
@@ -43,16 +43,16 @@ gulp.task('less', function () {
         .pipe(less({
         paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-        .pipe(gulp.dest('./css/sources'));
+        .pipe(gulp.dest('./public/css/sources'));
 });
 
 /*
 * Configuración de la tarea 'css' --> gulp-clean-css (gulp css)
 */
 gulp.task('css', function() {
-  return gulp.src('css/sources/style.css')
+  return gulp.src('./public/css/sources/style.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('css/dist'));
+    .pipe(gulp.dest('./public/css/dist'));
 });
 
 // Tarea 2 llamada minify-css
@@ -60,19 +60,19 @@ gulp.task('css', function() {
 * Configuración de la tarea 'minify-css' --> mincss (gulp mincss)
 */
 gulp.task('mincss', function () {
-    gulp.src('css/sources/*.css')
+    gulp.src('./public/css/sources/*.css')
         .pipe(concat('main.css'))
         .pipe(minifycss())
-        .pipe(gulp.dest('css/dist'))
+        .pipe(gulp.dest('./public/css/dist'))
 });
 
 /*
 * Configuración de la tarea 'img' --> gulp-imagemin (gulp img)
 */
 gulp.task('img', function () {
-    return gulp.src(['img/sources/**/*.*'])
+    return gulp.src(['img/**/*.*'])
         .pipe(imagemin())
-        .pipe(gulp.dest('img/dist'));
+        .pipe(gulp.dest('./public/img'));
 });
 
 /*
@@ -86,14 +86,14 @@ gulp.task('pug', function() {
       .pipe(pug({
         pretty: true
       }))
-      .pipe(gulp.dest('./'));
+      .pipe(gulp.dest('./public'));
 });
 
 /*
 * Configuración de la tarea 'sitemap' --> gulp-sitemap (gulp sitemap)
 */
 gulp.task('sitemap', function () {
-    gulp.src('*.html', {
+    gulp.src('public/**/*.html', {
             read: false
         })
         .pipe(sitemap({
@@ -101,5 +101,5 @@ gulp.task('sitemap', function () {
             changefreq: 'weekly',
             priority: '1.0'
         }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./public'));
 });
