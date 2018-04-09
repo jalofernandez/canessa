d4370e418c3344af2c25f6f33f8949f5b020dd74
jalofernandez <jalofernandez@gmail.com>
2018-04-09 16:43:48 +0200

    (SEO) Improvements in Service Worker file to avoid redirects bug

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,13 @@
 var CACHE_NAME = 'canessa-cache-v1';
 var urlsToCache = [
     '/',
-    '/index.html',
     '/css/main.css',
     '/js/jquery.min.js',
     '/js/materialize.min.js',
     '/js/scripts-min.js',
     '/img/logos/canessa-logo.svg',
     '/img/slider/afgano.jpg',
-    '/video/canessa-intro-2018-march.mp4',
-    '/contacto.html'
+    '/video/canessa-intro-2018-march.mp4'
 ];
 
 self.addEventListener('install', function(event) {
@@ -27,7 +25,39 @@ self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+            // return response || fetch(event.request);
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+
+            // IMPORTANT: Clone the request. A request is a stream and
+            // can only be consumed once. Since we are consuming this
+            // once by cache and once by the browser for fetch, we need
+            // to clone the response.
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+                function(response) {
+                    // Check if we received a valid response
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    // IMPORTANT: Clone the response. A response is a stream
+                    // and because we want the browser to consume the response
+                    // as well as the cache consuming the response, we need
+                    // to clone it so we have two streams.
+                    var responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                }
+            );
         })
     );
 });
