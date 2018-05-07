db7b3488cc4820d4dfc000b882e567b7187990e7
jalofernandez <jalofernandez@gmail.com>
2018-05-07 13:37:58 +0200

    [WIP - 07] New page Vanessa (about me) added + top  Dropdown with nav links

--- js/acm.js
+++ js/acm.js
@@ -18,8 +18,8 @@ $(document).ready(function(){
         inDuration: 300,
         outDuration: 225,
         constrain_width: false, // Does not change width of dropdown to that of the activator
-        // hover: true,  Activate on hover
-        gutter: 200, // Spacing from edge
+        hover: false,  // Activate on hover
+        gutter: 0, // Spacing from edge
         belowOrigin: false // Displays dropdown below the button
     });
     $('.dropdown-trigger').dropdown();
