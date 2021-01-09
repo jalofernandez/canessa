fbefb264498082a3a7d3f7bfa94863ce5d447e96
jalofernandez <jalofernandez@gmail.com>
2021-01-09 18:20:47 +0100

    📦 ✨ SASS package installed & configurated

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,12 +45,10 @@ export default {
     ]
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
-
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
+  css: [
+    // https://sass-lang.com
+    '@/assets/styles.sass'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
