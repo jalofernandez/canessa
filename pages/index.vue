25795d4da9bd44ba211f0597c32853c350f46140
jalofernandez <jalofernandez@gmail.com>
2021-01-13 19:26:33 +0100

    ✨ Video added in Home page intro hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,25 @@
 <template>
   <main>
-    <section class="hero is-light is-fullheight is-bold">
+    <section class="hero is-fullheight video is-dark is-bold">
+      <div class="hero-video">
+          <video
+            id="bgvid"
+            :poster="require(`~/assets/video/afgano.jpg`)"
+            :title="`Servicios ofrecidos en ${owner.copyright} en Valdemoro, Madrid`" 
+            playsinline autoplay muted loop
+          >
+            <source src="~/assets/video/canessa_web-promo-video_2018-09-15.mp4" type="video/mp4">
+          </video>
+      </div>
       <div class="hero-body">
-        <div class="container has-text-centered">
-          <h1 class="title">
-            The new standard in &lt;insert industry here&gt;
-          </h1>
-          <h2 class="subtitle">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </h2>
-        </div>
+          <div class="container">
+              // Text content goes here (h1 and call to action etc...)
+          </div>
+      </div>
+      <div class="hero-foot">
+          <div class="has-text-centered">
+              // Scroll down arrow here?
+          </div>
       </div>
     </section>
 
