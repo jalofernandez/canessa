729f2407cd74feeaf49856579a5a7b67408bd30c
jalofernandez <jalofernandez@gmail.com>
2021-01-16 19:50:31 +0100

    ✨  New layout component + ♻️  Mascotactiva & Vanessa pages

---
+++ components/SectionChessBoard.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="section">
+    <div :class="['container', 'mb-6', {'reflected': block.reflected}]" v-for="(block, index) in blocks" :key="index">
+      <div class="columns is-vcentered content is-medium">
+        <div class="column is-6">
+          <h2
+            :class="[
+              'is-size-4-mobile',
+              'has-text-primary',
+              'is-relative',
+              $mq == 'mobile' ? 'has-text-weight-bold' : 'has-text-weight-medium'
+            ]"
+          >
+            <span class="icon is-large th-quote th-is-open">
+              <i class="mdi mdi-48px mdi-format-quote-open"></i>
+            </span>
+            {{ block.title }}<em>...</em>
+          </h2>
+          <p class="is-size-6-mobile" v-html="block.description"></p>
+          <!-- (page) SERVICIOS -->
+          <NuxtLink
+            :to="{ name: block.to }"
+            :class="['th-link', { 'is-size-6-mobile': $mq == 'mobile' }]"
+            :title="`${block.link} en ${owner.copyright}`"
+            v-if="page == 'servicios'"
+          >
+            {{ block.link }}
+            <span class="icon">
+              <i class="mdi mdi-arrow-right"></i>
+            </span>
+          </NuxtLink>
+          <!-- (page) MASCOTACTIVA -->
+          <a
+            :class="['th-link', { 'is-size-6-mobile': $mq == 'mobile' }]"
+            href="tel:636515206"
+            :title="`${block.link} en ${owner.copyright}`"
+            rel="noopener noreferrer"
+            v-if="page == 'mascotactiva'"
+          >
+            {{ block.link }}: 636 515 206
+            <span class="icon">
+              <i class="mdi mdi-arrow-right"></i>
+            </span>
+          </a>
+        </div>
+        <div class="column">
+          <figure class="image">
+            <img
+              class="artwork"
+              :src="require(`~/assets/artworks/${block.img}`)"
+              :title="block.alt"
+              :alt="block.alt"
+              width="200"
+              height="200"
+            />
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionChessBoard',
+  props: {
+    blocks: {
+      type: Array,
+      default: null,
+      required: true,
+    },
+    page: {
+      type: String,
+      default: null,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      owner: this.$store.state.owner
+    }
+  }
+}
+</script>
--- pages/vanessavizcaya.vue
+++ pages/vanessavizcaya.vue
@@ -1,51 +1,6 @@
 <template>
   <main :class="$route.name">
-    <section class="section">
-      <div :class="['container', {'reflected': block.reflected}]" v-for="(block, index) in blocks" :key="index">
-        <div class="columns is-vcentered content is-medium">
-          <div class="column is-6">
-            <h2
-              :class="[
-                'is-size-4-mobile',
-                'has-text-primary',
-                'is-relative',
-                $mq == 'mobile' ? 'has-text-weight-bold' : 'has-text-weight-medium'
-              ]"
-            >
-              <span class="icon is-large th-quote th-is-open">
-                <i class="mdi mdi-48px mdi-format-quote-open"></i>
-              </span>
-              {{ block.title }}<em class="ml-1">...</em>
-            </h2>
-            <p class="is-size-6-mobile" v-html="block.description"></p>
-            <a
-              :class="['th-link', { 'is-size-6-mobile': $mq == 'mobile' }]"
-              href="tel:912480430"
-              :title="`${block.link} en ${owner.copyright}`"
-              :alt="`${block.link} en ${owner.copyright}`"
-              rel="noopener noreferrer"
-            >
-              {{ block.link }}: 91 248 04 30
-              <span class="icon">
-                <i class="mdi mdi-arrow-right"></i>
-              </span>
-            </a>
-          </div>
-          <div class="column">
-            <figure class="image">
-              <img
-                class="artwork"
-                :src="require(`~/assets/artworks/aboutme/${block.img}`)"
-                :title="block.alt"
-                :alt="block.alt"
-                width="200"
-                height="200"
-              />
-            </figure>
-          </div>
-        </div>
-      </div>
-    </section>
+    <SectionChessBoard :blocks="blocks" :page="$route.name"/>
     <TheFooter />
   </main>
 </template>
@@ -62,7 +17,7 @@ export default {
           title: 'Vanessa Vizcaya',
           description: '...Es una amante incondicional de los animales desde su infancia. <b>Empática como nadie con los sentimientos y las necesidades animales</b> pronto descubrió que tenía que dedicarse profesionalmente a ellos. Además siempre ha tenido perros y gatos como compañeros de vida asi que <b>sabe entenderlos a la perfección</b>.' + closeQuote,
           link: 'Prueba nuestros servicios',
-          img: 'vanessa-peluqueria-canina-canessa.png',
+          img: this.$route.name + '/vanessa-peluqueria-canina-canessa.png',
           alt: this.$store.state.owner.name + ' de ' + this.$store.state.owner.copyright + ', Madrid',
           reflected: false
         },
@@ -70,7 +25,7 @@ export default {
           title: this.yearsOfExperience(2009) + ' años de experiencia',
           description: '...Le avalan profesionalmente. En 2009 empieza a formarse como peluquera canina en la ISED. Tras este decidió completar <b>su formación con Mónica Calderón</b>, toda una institución en el sector de la peluquería canina. Los comienzos fueron servicios a domicilio pero pronto avanzó a centros veterinarios y peluquerías caninas.' + closeQuote,
           link: 'Pide cita conmigo',
-          img: 'vanessa-tijeras-peluqueria-canina-canessa.png',
+          img: this.$route.name + '/vanessa-tijeras-peluqueria-canina-canessa.png',
           alt: 'Ilustración tijeras en ' + this.$store.state.owner.copyright + ', Valdemoro, Madrid',
           reflected: true
         },
@@ -78,7 +33,7 @@ export default {
           title: 'Peluquería Canessa',
           description: '...Comienza a funcionar como reemplazo de la antigua KTC en Marzo de 2017 siendo la principal tarea como peluquera canina <b>cuidar el pelaje de los perros, mejorar su higiene y procurarles un fantástico corte de pelo</b>. Esta es una tarea manual y artesana realizada con el máximo cariño y respeto en la que lo más importante es el bienestar de los animales a los que cuida y que le resulta tan gratificante.' + closeQuote,
           link: 'Reserva hora en',
-          img: 'vanessa-canessa-peluqueria-canina-canessa.png',
+          img: this.$route.name + '/vanessa-canessa-peluqueria-canina-canessa.png',
           alt: 'Ilustración Canessa en ' + this.$store.state.owner.copyright + ', Madrid',
           reflected: false
         }
