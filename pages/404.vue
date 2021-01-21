172deabd7e3802b06b9caccfc193ef1105b9dd07
jalofernandez <jalofernandez@gmail.com>
2021-01-21 19:17:48 +0100

    ♻️ 404 page with new copies as tips

--- pages/404.vue
+++ pages/404.vue
@@ -1,6 +1,21 @@
 <template>
   <main :class="$route.name">
     <SectionHero :hero="hero" :page="$route.name" />
+    <section class="hero is-small">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div
+              class="column is-9 is-offset-3 is-three-quarters-tablet is-offset-one-quarter-tablet is-offset-one-fifth-mobile"
+            >
+              <p class="is-size-5 is-size-6-mobile" v-for="(tip, index) in tips" :key="index">
+                ...{{ tip }}.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <TheFooter />
   </main>
 </template>
@@ -20,7 +35,13 @@ export default {
           width: 300,
           height: 300
         }
-      }
+      },
+      tips: [
+        'Que haya tecleado incorrectamente la dirección URL',
+        'Que haya perdido su conexión a internet',
+        'Que la página esté obsoleta y haya sido eliminada',
+        'Que la página no exista en el idioma que solicita'
+      ]
     }
   },
   head() {
