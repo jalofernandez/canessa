375aac54549b55e7cc81000a3909de1806913185
jalofernandez <jalofernandez@gmail.com>
2021-01-22 19:09:11 +0100

    🚧  Piensos new page in progress...

---
+++ components/BoxItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="box" v-for="(item, index) in items" :key="index">
+      <figure class="image block">
+        <img
+          :src="require(`~/assets/images/piensos/${item.img}`)"
+          :alt="`Advance ${item.title} de Affinity`"
+          :title="`Advance ${item.title} de Affinity`"
+          width="222"
+          height="222"
+        >
+      </figure>
+      <p class="is-size-6 block">
+        {{ item.title }}:
+      </p>
+      <p class="is-size-6 block" v-for="(detail, index) in item.details" :key="index">
+        <span class="">Bolsa de <b>{{ detail.weight }}</b></span>
+        <span class="tag is-link is-medium">{{ detail.price }} €</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoxItem',
+  props: {
+    items: {
+      type: Array,
+      default: null,
+      required: true,
+    },
+  }
+}
+</script>
---
+++ pages/piensos.vue
@@ -0,0 +1,203 @@
+<template>
+  <main :class="$route.name">
+    <SectionHero :hero="hero" :page="$route.name" />
+    <div class="tabs is-large is-centered">
+      <ul>
+        <li :class="{ 'is-active': !isLibra }">
+          <a href="" @click.prevent="toggleTabs()">
+            <span>Advance</span>
+          </a>
+        </li>
+        <li :class="{ 'is-active': isLibra }">
+          <a href="" @click.prevent="toggleTabs()">
+            <span>Libra</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- ADVANCE -->
+    <transition name="canessa-transition" appear>
+      <div :class="['container', { 'has-text-centered': $mq == 'mobile' }]" v-if="!isLibra">
+        <div class="grid">
+          <BoxItem :items="piensos.advance" />
+        </div>
+      </div>
+    </transition>
+    <!-- LIBRA -->
+    <transition name="canessa-transition" appear>
+      <div :class="['container', { 'has-text-centered': $mq == 'mobile' }]" v-if="isLibra">
+        <div class="grid">
+          <BoxItem :items="piensos.libra" />
+        </div>
+      </div>
+    </transition>
+    <TheFooter />
+  </main>
+</template>
+
+<script>
+// import piensos from "~/data/piensos"
+
+export default {
+  // asyncData() {
+  //   return new Promise((resolve) => {
+  //     setTimeout(function () {
+  //       resolve({ piensos: piensos })
+  //     }, 1000)
+  //   })
+  // },
+  data() {
+    return {
+      isLibra: false,
+      owner: this.$store.state.owner,
+      pages: this.$store.state.pages,
+      hero: {
+        title: 'Pienso y alimentación canina',
+        subtitle:
+          'Trabajamos con las <span class="has-text-weight-bold">mejores marcas</span> para alimentar a tu mejor amigo',
+        img: {
+          src: 'advance-logo.png',
+          width: 200,
+          height: 77
+        }
+      },
+      piensos: {
+        advance: [
+          {
+            title: 'French Bulldog Adult',
+            img: 'advange-french-bulldog.png',
+            details: [
+              { weight: '3 kg', price: '19,99' }
+            ]
+          },
+          {
+            title: 'Yorkshire terrier de hasta 10 meses de pollo y arroz',
+            img: 'advange-yorkshire-terrier.png',
+            details: [
+              { weight: '1,5 kg', price: '12,99' }
+            ]
+          },
+          {
+            title: 'Cachorro Mini de hasta 10 meses de pollo y arroz',
+            img: 'advange-puppy-mini.png',
+            details: [
+              { weight: '1,5 kg', price: '12,99' }
+            ]
+          },
+          {
+            title: 'Sensitive para cachorros de hasta 12 meses de pollo y salmón',
+            img: 'advange-puppy-sensitive-salmon-rice.png',
+            details: [
+              { weight: '800 g', price: '5,99' },
+              { weight: '3 kg', price: '19,99' }
+            ]
+          },
+          {
+            title: 'Medium 7 años de pollo y salmón',
+            img: 'advange-medium-7years.png',
+            details: [
+              { weight: '3 kg', price: '19,99' }
+            ]
+          },
+          {
+            title: 'Medium Adult de pollo con arroz',
+            img: 'advange-medium-adult.png',
+            details: [
+              { weight: '3 kg', price: '16,50' },
+              { weight: '15 kg', price: '49,99' }
+            ]
+          },
+          {
+            title: 'Medium Light de pollo y arroz',
+            img: 'advange-medium-light.png',
+            details: null
+          },
+          {
+            title: 'Mini Adult de pollo y arroz',
+            img: 'advange-mini-adult.png',
+            details: [
+              { weight: '800 g', price: '4,99' },
+              { weight: '3 kg', price: '16,50' },
+              { weight: '15 kg', price: '31,95' }
+            ]
+          },
+          {
+            title: 'Mini Light de pollo y arroz',
+            img: 'advange-mini-light.png',
+            details: [
+              { weight: '800 g', price: '4,99' },
+              { weight: '3 kg', price: '19,99' }
+            ]
+          },
+          {
+            title: 'Mini Sensitive de pollo y salmón',
+            img: 'advange-mini-sensitive-salmon-rice.png',
+            details: null
+          }
+        ],
+        libra: [
+          {
+            title: 'Pollo para adultos',
+            img: 'affinity-libra-adult-chicken.png',
+            details: [
+              { weight: '3 kg', price: '6,95' },
+              { weight: '15 kg', price: '26,99' }
+            ]
+          },
+          {
+            title: 'Cordero para adultos',
+            img: 'affinity-libra-adult-lamb.png',
+            details: [
+              { weight: '3 kg', price: '6,95' }
+            ]
+          },
+          {
+            title: 'Pollo con arroz para cachorros',
+            img: 'affinity-libra-puppy-pollo-arroz.png',
+            details: [
+              { weight: '3 kg', price: '7,95' }
+            ]
+          },
+          {
+            title: 'Salmón para cachorros',
+            img: 'affinity-libra-puppy-salmon.png',
+            details: [
+              { weight: '3 kg', price: '7,95' }
+            ]
+          }
+        ],
+      }
+    }
+  },
+  head() {
+    const title = this.hero.title + ' en'
+    const description =
+      'Página de error del sitio web ' + this.owner.copyright + '. Pruebe otra URL o vaya a la página de inicio para encontrar la descripción de nuestros servicios y contacto.'
+    const canonical = 'https://' + this.owner.url + this.$route.path
+
+    return {
+      title,
+      meta: [
+        { hid: 'description', name: 'description', content: description },
+        { hid: 'Classification', name: 'Classification', content: 'Business' },
+        { hid: 'subject', name: 'subject', content: description },
+      ],
+      // Structured Data (Schema)
+      __dangerouslyDisableSanitizers: ['script'],
+      script: [
+        { innerHTML: JSON.stringify(this.owner.schema), type: 'application/ld+json' },
+      ],
+      link: [
+        { rel: 'canonical', hid: 'canonical', href: canonical },
+        { rel: 'alternate', hid: 'alternate', href: canonical+'/#!' },
+        { rel: 'alternate', hid: 'alternate', href: canonical+'#!' },
+      ]
+    }
+  },
+  methods: {
+    toggleTabs() {
+      this.isLibra = !this.isLibra
+    }
+  }
+}
+</script>
