fbd8fba4fdee18898980fe2bc1b480576aba5b25
jalofernandez <jalofernandez@gmail.com>
2021-03-08 19:04:49 +0100

    ⬆️Project version updated to 2.0.3

--- store/owner.js
+++ store/owner.js
@@ -12,7 +12,7 @@ export const state = () => ({
   phone: 912480430,
   author: 'jalofernandez',
   currentYear: new Date().getFullYear(),
-  version: '2.0.2',
+  version: '2.0.3',
   partner: 'haztunegociodigital',
   schema: {
     '@context': 'http://schema.org',
