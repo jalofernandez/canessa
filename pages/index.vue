a079c7f32528cad52ffe420f1654c9acd34ba4dd
jalofernandez <jalofernandez@gmail.com>
2021-04-25 13:20:03 +0200

    🔥 Useless & commented HTML code in Index page removed

--- pages/index.vue
+++ pages/index.vue
@@ -57,19 +57,6 @@
                 <small>{{ review.comment }}</small>
               </p>
             </div>
-            <!-- <nav class="level is-mobile">
-              <div class="level-left">
-                <a class="level-item">
-                  <span class="icon is-small"><i class="fas fa-reply"></i></span>
-                </a>
-                <a class="level-item">
-                  <span class="icon is-small"><i class="fas fa-retweet"></i></span>
-                </a>
-                <a class="level-item">
-                  <span class="icon is-small"><i class="fas fa-heart"></i></span>
-                </a>
-              </div>
-            </nav> -->
           </div>
         </article>
         <!-- <div class="columns features">
